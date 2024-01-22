c902ec2f8d5378be55e00f39d2e60c4712894e53
Rakesh Patel <rakesh246808@gmail.com>
2024-01-22 19:09:37 +0530

    added some styling

--- script.js
+++ script.js
@@ -24,65 +24,63 @@ generateTextBtn.addEventListener('click', function(event)
     });
 });
 
-generateImageBtn.addEventListener('click', function(event){
+// generateImageBtn.addEventListener('click', function(event){
 
-    var image = '';
+//     var image = '';
 
 
-$(document).ready(function(){
-    var apiKey = "ZjAcPEQaP92mxu2K0dR8H9jMoi3HWFu8t54ev8WJjCSikFfOKoazGgI4";
+// $(document).ready(function(){
+//     var apiKey = "ZjAcPEQaP92mxu2K0dR8H9jMoi3HWFu8t54ev8WJjCSikFfOKoazGgI4";
 
-    imageSearch();
-    $(".img").remove();
-    function imageSearch()
-    {
+//     imageSearch();
+//     $(".img").remove();
+//     function imageSearch()
+//     {
         
-        $("#images").innerHTML = '';
-        // var search = $('#search').val();
-        let search ='fire';
-        let motivationPhoto = Math.round((Math.random() * 1) + (Math.random() * 70));
+//         $("#images").innerHTML = '';
+//         // var search = $('#search').val();
+//         let search ='fire';
+//         let motivationPhoto = Math.round((Math.random() * 1) + (Math.random() * 70));
 
-        $.ajax({
-            method: 'GET',
-            beforeSend: function (xhr){
-                xhr.setRequestHeader("Authorization", apiKey);
-            },
+//         $.ajax({
+//             method: 'GET',
+//             beforeSend: function (xhr){
+//                 xhr.setRequestHeader("Authorization", apiKey);
+//             },
 
-            url: `https://api.pexels.com/v1/search?query="+${search}+"&per_page=1000&page=1`,
-            success: function(data)
-            {
-                console.log(data);
-                // data.photos.forEach(photo => {
-                //     image = `
+//             url: `https://api.pexels.com/v1/search?query="+${search}+"&per_page=1000&page=1`,
+//             success: function(data)
+//             {
+//                 console.log(data);
+//                 // data.photos.forEach(photo => {
+//                 //     image = `
 
-                //     <img src="${photo.src.original}" width="400" class="img" height = "300"/>
+//                 //     <img src="${photo.src.original}" width="400" class="img" height = "300"/>
                     
-                //     `;
-                //     $("#images").append(image);
+//                 //     `;
+//                 //     $("#images").append(image);
 
-                // });
-                let r = data.photos[motivationPhoto];
+//                 // });
+//                 let r = data.photos[motivationPhoto];
                 
-                // image =`<img src="${r.src.original}" width="1920" class="img" height = "1080" background-size="cover" position="absolute"/>`
-                // $("#images").append(image);
-                // $("#images").src= r.src.original;
-                $("#images").attr("src",r.src.original);
-                console.log(r);
-            },
-            error:function(error){
-                console.log(error);
-            }
-            // url:"https://www.pexels.com/photo/brown-rocks-during-golden-hour-2014422/"
-        })
+//                 // image =`<img src="${r.src.original}" width="1920" class="img" height = "1080" background-size="cover" position="absolute"/>`
+//                 // $("#images").append(image);
+//                 // $("#images").src= r.src.original;
+//                 $("#images").attr("src",r.src.original);
+//                 console.log(r);
+//             },
+//             error:function(error){
+//                 console.log(error);
+//             }
+//             // url:"https://www.pexels.com/photo/brown-rocks-during-golden-hour-2014422/"
+//         })
        
         
-    }
-})
+//     }
+// });
 
 
-
-
-});
+// });
 
 
 
