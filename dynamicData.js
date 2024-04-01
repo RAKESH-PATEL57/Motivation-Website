46ba4f68bee77189f20b8eefe18adf4f5c92d059
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2024-04-01 09:50:53 +0530

    Update dynamicData.js

--- dynamicData.js
+++ dynamicData.js
@@ -1,39 +1,41 @@
 // creating dynamic datas or card from json file
-
-let allMotivationNames = "./ApiList.json";
+"use strict"
+import test from "./ApiList.json" assert {type: 'json'};
+for(let i = 0 ; i< test.length; i++)
+{
+    // console.log(test[i]);
+    gettingDataFromJson(test[i].link);
+}
 
 let cardContainer = document.querySelector(".cards-container");
 
 
 
-async function gettingDataFromJson()
+async function gettingDataFromJson(allMotivationData)
 {
-    const apiData = await fetch(allMotivationNames);
+    const apiData = await fetch(allMotivationData);
     const allMotivationNamesArray = await apiData.json();
+
+    // console.log(allMotivationNamesArray);
+
 
     let length = allMotivationNamesArray.length;
 
 
-    for(let i = 0 ; i < length ; i++ )
-    {
-
-        const dataFromLink = await fetch(allMotivationNamesArray[i].link);
-        const allDataFromLink = await dataFromLink.json();
     
-        console.log(allDataFromLink);
+        // console.log(allDataFromLink);
 
-        const cards = `<div class="card-details">
+        const cards = `<div class="card-details skelaton">
         <div class="cdheading" id="womenMotvOpen">
             <h1>Women<br> Motivation</h1>
             <h2>Open</h2>
         </div>
-        <img loading="lazy" src="${(allDataFromLink[0].image).slice(1)}" alt="MotivationImage">
+        <img loading="lazy" src="${(allMotivationNamesArray[0].image).slice(0)}" alt="MotivationImage">
         </div>`
         cardContainer.insertAdjacentHTML("beforeend", cards);
-    }
+    
 }
 
-gettingDataFromJson();
 
 
 
