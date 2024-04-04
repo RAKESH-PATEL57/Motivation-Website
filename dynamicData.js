b9ed7c090e3044fba7d5e504729bc2896caafc87
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2024-04-04 21:47:59 +0530

    Update dynamicData.js

--- dynamicData.js
+++ dynamicData.js
@@ -1,42 +1,136 @@
 // creating dynamic datas or card from json file
 "use strict"
-import test from "./ApiList.json" assert {type: 'json'};
-for(let i = 0 ; i< test.length; i++)
-{
-    // console.log(test[i]);
-    gettingDataFromJson(test[i].link);
-}
+import apiList from "./ApiList.json" with {type: 'json'};
+let nextBtn = document.querySelector("#nextBtn");
+let prevBtn = document.querySelector("#prevBtn");
 
-let cardContainer = document.querySelector(".cards-container");
+let firstbrd = document.querySelector(".motivationCardContainer .firstbrd");
+let secondbrd = document.querySelector(".motivationCardContainer .secondbrd");
+let thirdbrd = document.querySelector(".motivationCardContainer .thirdbrd");
+let fourthbrd = document.querySelector(".motivationCardContainer .fourthbrd");
 
+var apiLength = apiList.length;
 
+let motivationCardContainer = document.querySelector(".motivationCardContainer");
+
+gettingDataFromJson(apiList[0]);  // for showing the first motivation
+var count = 1;
+
+nextBtn.addEventListener("click", () => {
+    if(count == apiLength-1)
+    {
+        gettingDataFromJson(apiList[count]);
+        count = 0;
+    }
+    else
+    {
+        gettingDataFromJson(apiList[count]);
+        count++;
+    }
+});
+
+prevBtn.addEventListener("click", () => {
+    if(count == 0)
+    {
+        gettingDataFromJson(apiList[count]);
+        count = apiLength-1;
+    }
+    else
+    {
+        gettingDataFromJson(apiList[count]);
+        count--;
+    }
+});
+
+var test = 0;
 
 async function gettingDataFromJson(allMotivationData)
 {
-    const apiData = await fetch(allMotivationData);
+    const apiData = await fetch(allMotivationData.link);
     const allMotivationNamesArray = await apiData.json();
 
-    // console.log(allMotivationNamesArray);
+
+    let length = allMotivationNamesArray.length;  
+    let imageSource = (allMotivationNamesArray[0].image).slice(1);
+
+    loadingMotv(length, allMotivationData);
+        
+    }
 
 
-    let length = allMotivationNamesArray.length;
+function loadingMotv(length, allMotivationData)
+{
+    firstbrd.classList.add("loadFirstbrd");
+    secondbrd.classList.add("loadSecondbrd");
+    thirdbrd.classList.add("loadThirdbrd");
+    fourthbrd.classList.add("loadFourthbrd");
 
-
-    
-        // console.log(allDataFromLink);
-
-        const cards = `<div class="card-details skelaton">
-        <div class="cdheading" id="womenMotvOpen">
-            <h1>Women<br> Motivation</h1>
-            <h2>Open</h2>
+    setTimeout(() => {
+        const cards = `<div class="motivationCard">
+        <div class="mName">
+        <h1 class="changeName">${allMotivationData.catagory}</h1>
+        <h1>Motivation</h1>
         </div>
-        <img loading="lazy" src="${(allMotivationNamesArray[0].image).slice(0)}" alt="MotivationImage">
+        <button class="openBtn">Open</button>
         </div>`
-        cardContainer.insertAdjacentHTML("beforeend", cards);
-    
+        motivationCardContainer.insertAdjacentHTML("beforeend", cards);  //added dynamic data
+
+        let motivationCards = document.querySelectorAll(".motivationCard");
+        let motivationName = document.querySelectorAll(".mName");
+
+        console.log(motivationCards.length);
+        console.log(test);
+        if(test > 0)
+        {
+            let data = motivationCards[test-1];
+            console.log(data);
+            data.style.display = 'none';
+            console.log('rakesh');
+        }
+        
+        test++;
+        firstbrd.classList.remove("loadFirstbrd");
+        secondbrd.classList.remove("loadSecondbrd");
+        thirdbrd.classList.remove("loadThirdbrd");
+        fourthbrd.classList.remove("loadFourthbrd");
+
+    },500);
 }
 
+// function cardChangeAnimation(motvName) 
+// { 
+//     console.log(motvName);
 
-
-
-
+//     firstbrd.classList.add("loadFirstbrd");
+//     secondbrd.classList.add("loadSecondbrd");
+//     thirdbrd.classList.add("loadThirdbrd");
+//     fourthbrd.classList.add("loadFourthbrd");
+//     // openBtn[changeCard].style.bottom = "50%";
+//     // openBtn[changeCard].style.translate = "0%, -50%";
+//     // openBtn[changeCard].style.scale = 0;
+//     // motvName.style.border = '2px solid aqua';
+//     // images[changeCard].style.scale = 0;
+//     // images[changeCard].style.opacity = 0;
+//     // allCardslist[changeCard].style.rotate = "360deg";
+//     setTimeout(() => {
+//         firstbrd.classList.remove("loadFirstbrd");
+//         secondbrd.classList.remove("loadSecondbrd");
+//         thirdbrd.classList.remove("loadThirdbrd");
+//         fourthbrd.classList.remove("loadFourthbrd");
+//         // openBtn[changeCard].style.bottom = "1%";
+//         // openBtn[changeCard].style.translate = "0%, 0%";
+//         // openBtn[changeCard].style.scale = 1;
+//         // motvName.style.scale = 0;
+//         // images[changeCard].style.scale = 1;
+//         // images[changeCard].style.opacity = ".3";
+//         // allCardslist[changeCard].style.rotate = "0deg";
+//     },300);
+// }
+    
+    // <h1>${allMotivationNamesArray[0].category}</h1>
+    
+    // <img class="image" src="${imageSource}" alt="">
+    
+    
+    
+    
