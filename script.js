f77e1d288c513894e3c3919ff9e142442ffd915b
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2024-04-10 19:42:55 +0530

    deleted unwanted lines of codes

--- script.js
+++ script.js
@@ -9,29 +9,6 @@ let introPage = document.querySelector(".intropage");
 let mainContainer = document.querySelector("main");
 let footer = document.querySelector("footer");
 
-// window.addEventListener('load', () => {
-//     loadingIntro.style.height = "100vh";    
-//     loadingIntro.style.width = "100vw";  
-//     // loadingIntro.style.display = "none";  
-//     loadingIntro.style.zIndex = 0;  
-//     loadingIntro.style.transition = ".4s";  
-    
-//     leftTop.style.animation = "none";
-//     leftTop.style.opacity = 0;
-//     leftBottom.style.animation = "none";
-//     leftBottom.style.opacity = 0;
-//     rightTop.style.animation = "none";
-//     rightTop.style.opacity = 0;
-//     rightBottom.style.animation = "none";
-//     rightBottom.style.opacity = 0;
-    
-//     headerContainer.style.display = "flex";
-//     introPage.style.opacity = 1;
-//     mainContainer.style.display = "flex";
-//     footer.style.display = "flex";
-
-// })
-
 setTimeout( () =>{
     loadingIntro.style.height = "100vh";    
     loadingIntro.style.width = "100vw";  
